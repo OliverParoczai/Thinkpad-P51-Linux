c75215c2ccd970a250678e21a176738d21cad3d4
Paróczai Olivér <paroczai.oliver@protonmail.com>
2020-10-13 22:25:36 +0200

    Create StartExtDP.sh

---
+++ home/Desktop/StartExtDP.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+konsole -e ~/Scripts/ExternalDisplay/Start.sh #Replace 'konsole' with your terminal of choice, just needed for a quick exit using Ctrl+C
+
+~/Scripts/ExternalDisplay/Stop.sh
